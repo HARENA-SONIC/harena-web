5ce0040a636b2e3ebe56f49f3b169e70d498f4e7
Abi-Nf <hei.abigail.3@gmail.com>
2024-07-19 12:12:29 +0300

    chore: add cypress testing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:cypress/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
