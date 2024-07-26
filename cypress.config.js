62f7de85269e00c9e6a090a3fb2f9e1e8863bfec
RickaPrincy <rckprincy@gmail.com>
2024-07-27 01:17:37 +0300

    chore: cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,56 @@
+import taskCoverage from '@cypress/code-coverage/task';
 import { defineConfig } from 'cypress';
+import mergeReport from 'cypress-sonarqube-reporter/mergeReports';
 
-const REQUEST_TIMEOUT = 10000;
+const REQUEST_TIMEOUT = 15_000;
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: ['cypress/**/*', 'src/**/*.cy.*'],
+    },
+  },
+  video: false,
+
+  retries: {
+    runMode: 3,
+    openMode: 0,
+  },
+
+  viewportWidth: 2014,
+  viewportHeight: 844,
+  defaultCommandTimeout: 30000,
+
+  projectId: '4f6tz2',
+
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-sonarqube-reporter',
+    mergeFileName: 'test-reports.xml',
+    cypressSonarqubeReporterReporterOptions: {
+      overwrite: true,
+    },
+  },
   e2e: {
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {
+      taskCoverage(on, config);
+      on('after:run', (result) => {
+        mergeReport(result, { mergeFileName: 'test-reports.xml' });
+      });
+      return config;
     },
     baseUrl: 'http://localhost:5173/#',
     requestTimeout: REQUEST_TIMEOUT,
   },
-
   component: {
+    setupNodeEvents(on, config) {
+      taskCoverage(on, config);
+      on('after:run', (result) => {
+        mergeReport(result, { mergeFileName: 'test-reports.xml' });
+      });
+      return config;
+    },
+    specPattern: 'src/**/*.cy.{js,ts,jsx,tsx}',
     devServer: {
       framework: 'react',
       bundler: 'vite',
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import istanbul from 'vite-plugin-istanbul';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [react()],
     define: {
       'process.env': env,
     },
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      istanbul({
+        cypress: true,
+        requireEnv: false,
+        include: 'src/*',
+        exclude: ['node_modules', 'cypress'],
+      }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
